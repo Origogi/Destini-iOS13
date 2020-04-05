c44b791950c8ee2317c2073d0dcff4cdabb7a9b6
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-04-05 21:47:00 +0900

    [Implement] Complete source

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var storyLabel: UILabel!
+    @IBOutlet var choice1Button: UIButton!
+    @IBOutlet var choice2Button: UIButton!
 
-    @IBOutlet weak var storyLabel: UILabel!
-    @IBOutlet weak var choice1Button: UIButton!
-    @IBOutlet weak var choice2Button: UIButton!
-    
+    var storyBrain = StoryBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let currentStory = storyBrain.getCurrentStory()
 
+        if currentStory.choice1 == sender.currentTitle {
+            storyBrain.nextStory(destination: currentStory.choice1Destination)
+        }
+        else {
+            storyBrain.nextStory(destination: currentStory.choice2Destination)
+        }
+
+        updateUI()
+    }
+
+    func updateUI() {
+        let currentStory = storyBrain.getCurrentStory()
+
+        storyLabel.text = currentStory.title
+        choice1Button.setTitle(currentStory.choice1, for: .normal)
+        choice2Button.setTitle(currentStory.choice2, for: .normal)
+    }
 }
-
